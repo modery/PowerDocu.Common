55bd431522a4af9c9744a879774fbe58a2506386
Rene Modery <rene@modery.net>
2022-06-11 18:36:10 +0800

    Initial commit
    Brought over from PowerDocu

---
+++ PowerDocu.Common/ConnectionReference.cs
@@ -0,0 +1,18 @@
+using System;
+using PowerDocu.Common;
+
+namespace PowerDocu.Common
+{
+    public class ConnectionReference
+    {
+        public string Name;
+        public string Source;
+        public string ID;
+        public string ConnectionReferenceLogicalName;
+        public ConnectionType Type;
+
+        public ConnectionReference()
+        {
+        }
+    }
+}
---
+++ PowerDocu.Common/ControlEntity.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+namespace PowerDocu.Common
+{
+    public class ControlEntity
+    {
+        public string ID;
+        public string Name;
+        public string Type;
+        public List<Expression> Properties = new List<Expression>();
+        public List<ControlEntity> Children = new List<ControlEntity>();
+        public List<Rule> Rules = new List<Rule>();
+        public ControlEntity Parent;
+        public ControlEntity()
+        {
+        }
+
+        public ControlEntity Screen()
+        {
+            if (this.Type == "screen") return this;
+            if (Parent != null) return Parent.Screen();
+            return null;
+        }
+    }
+
+    public class Rule
+    {
+        public string Property;
+        public string Category;
+        public string InvariantScript;
+        public string RuleProviderType;
+    }
+}
