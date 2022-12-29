e813fd12e954d6ef78c0248c038cc12548bb1bf6
Rene Modery <rene@modery.net>
2022-12-29 15:35:17 +0800

    Adding the class to the PowerDocu.Common namespace

--- PowerDocu.Common/JsonUtil.cs
+++ PowerDocu.Common/JsonUtil.cs
@@ -2,15 +2,18 @@ using System;
 using System.IO;
 using Newtonsoft.Json;
 
-class JsonUtil
+namespace PowerDocu.Common
 {
-    public static string JsonPrettify(string json)
+    static class JsonUtil
     {
-        using StringReader stringReader = new StringReader(json);
-        using var stringWriter = new StringWriter();
-        var jsonReader = new JsonTextReader(stringReader);
-        var jsonWriter = new JsonTextWriter(stringWriter) { Formatting = Formatting.Indented };
-        jsonWriter.WriteToken(jsonReader);
-        return stringWriter.ToString();
+        public static string JsonPrettify(string json)
+        {
+            using StringReader stringReader = new StringReader(json);
+            using var stringWriter = new StringWriter();
+            var jsonReader = new JsonTextReader(stringReader);
+            var jsonWriter = new JsonTextWriter(stringWriter) { Formatting = Formatting.Indented };
+            jsonWriter.WriteToken(jsonReader);
+            return stringWriter.ToString();
+        }
     }
 }
