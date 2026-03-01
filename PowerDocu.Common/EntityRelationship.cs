10be16be32a29c200bccae2d7a32c1ab4c5c0be6
Rene Modery <rene@modery.net>
2026-03-01 14:32:33 +0800

    Additional table relationships getting parsed

--- PowerDocu.Common/EntityRelationship.cs
+++ PowerDocu.Common/EntityRelationship.cs
@@ -38,7 +38,58 @@ namespace PowerDocu.Common
 
         public string getReferencingAttributeName() {
             return xmlEntity.SelectSingleNode("ReferencingAttributeName")?.InnerText ?? "";
+        }
 
+        public string getIntersectEntityName() {
+            return xmlEntity.SelectSingleNode("IntersectEntityName")?.InnerText ?? "";
+        }
+
+        public bool getIsCustomizable() {
+            return xmlEntity.SelectSingleNode("IsCustomizable")?.InnerText.Equals("1") ?? false;
+        }
+
+        public string getIntroducedVersion() {
+            return xmlEntity.SelectSingleNode("IntroducedVersion")?.InnerText ?? "";
+        }
+
+        public bool getIsHierarchical() {
+            return xmlEntity.SelectSingleNode("IsHierarchical")?.InnerText.Equals("1") ?? false;
+        }
+
+        public bool getIsValidForAdvancedFind() {
+            return xmlEntity.SelectSingleNode("IsValidForAdvancedFind")?.InnerText.Equals("1") ?? false;
+        }
+
+        public string getCascadeAssign() {
+            return xmlEntity.SelectSingleNode("CascadeAssign")?.InnerText ?? "";
+        }
+
+        public string getCascadeDelete() {
+            return xmlEntity.SelectSingleNode("CascadeDelete")?.InnerText ?? "";
+        }
+
+        public string getCascadeReparent() {
+            return xmlEntity.SelectSingleNode("CascadeReparent")?.InnerText ?? "";
+        }
+
+        public string getCascadeShare() {
+            return xmlEntity.SelectSingleNode("CascadeShare")?.InnerText ?? "";
+        }
+
+        public string getCascadeUnshare() {
+            return xmlEntity.SelectSingleNode("CascadeUnshare")?.InnerText ?? "";
+        }
+
+        public string getCascadeArchive() {
+            return xmlEntity.SelectSingleNode("CascadeArchive")?.InnerText ?? "";
+        }
+
+        public string getCascadeRollupView() {
+            return xmlEntity.SelectSingleNode("CascadeRollupView")?.InnerText ?? "";
+        }
+
+        public string getDescription() {
+            return xmlEntity.SelectSingleNode("RelationshipDescription/Descriptions/Description")?.Attributes.GetNamedItem("description")?.InnerText ?? "";
         }
     }
 }
