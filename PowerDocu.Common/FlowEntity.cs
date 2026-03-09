3f17b51f9259a519bf16d91b9cc8c3b61ccc1dc5
Rene Modery <rene@modery.net>
2026-03-09 13:04:08 +0800

    ModernFlowType added to FlowEntity

--- PowerDocu.Common/FlowEntity.cs
+++ PowerDocu.Common/FlowEntity.cs
@@ -12,10 +12,29 @@ namespace PowerDocu.Common
             Unknown
         }
 
+        // https://learn.microsoft.com/en-us/power-apps/developer/data-platform/reference/entities/workflow#BKMK_ModernFlowType
+        public enum ModernFlowType
+        {
+            CloudFlow = 0,
+            AgentFlow = 1,
+            M365CopilotAgentFlow = 2
+        }
+
+        public static string GetModernFlowTypeLabel(ModernFlowType type)
+        {
+            return type switch
+            {
+                ModernFlowType.AgentFlow => "Agent Flow",
+                ModernFlowType.M365CopilotAgentFlow => "M365 Copilot Agent Flow",
+                _ => "Cloud Flow"
+            };
+        }
+
         public string ID;
         public string Name;
         public string Description;
         public FlowType flowType;
+        public ModernFlowType modernFlowType = ModernFlowType.CloudFlow;
         public Trigger trigger;
         public ActionGraph actions = new ActionGraph();
         public List<ConnectionReference> connectionReferences = new List<ConnectionReference>();
